16eb7f4974148e99d6edf054d95f9d79ab3e348b
iamyuuk <2704804982@qq.com>
2023-07-15 22:52:36 +0800

    极限の性能优化

--- src/main/java/org/encinet/maxplayer/MaxPlayer.java
+++ src/main/java/org/encinet/maxplayer/MaxPlayer.java
@@ -1,22 +1,20 @@
 package org.encinet.maxplayer;
-
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.YamlConfiguration;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
-
 import java.io.File;
 import java.io.IOException;
-
+import java.util.Objects;
 public final class MaxPlayer extends JavaPlugin implements Listener {
     private File configFile;
     private YamlConfiguration config;
+    private int maxOnlinePlayers;
     @Override
     public void onEnable() {
         configFile = new File(getDataFolder(), "config.yml");
@@ -25,41 +23,33 @@ public final class MaxPlayer extends JavaPlugin implements Listener {
             config.set("maxOnlinePlayers", 0);
         }
         getServer().getPluginManager().registerEvents(this, this);
+        Objects.requireNonNull(getCommand("maxplayers")).setExecutor(this);
         getLogger().info("插件已开启 这是一个用来记录服务器最大在线人数的插件");
         check();
     }
     @Override
     public void onDisable() {
+        config.set("maxOnlinePlayers", maxOnlinePlayers);
+        try {
+            config.save(configFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         getLogger().info("插件已关闭");
     }
     @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent event) {
-        check();
-    }
+    public void onPlayerJoin(PlayerJoinEvent event) { check(); }
     @Override
-    public boolean onCommand(@NotNull CommandSender sender, Command command, @NotNull String label, String[] args) {
-        if (command.getName().equalsIgnoreCase("maxplayers")) {
-            int currentMaxPlayers = config.getInt("maxOnlinePlayers");
-            sender.sendMessage("服务器的最高在线人数是：" + currentMaxPlayers);
-            return true;
-        }
-        return false;
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
+        sender.sendMessage("服务器的最高在线人数是：" + maxOnlinePlayers);
+        return true;
     }
     private void check() {
-        int maxOnlinePlayers = config.getInt("maxOnlinePlayers");
         int onlinePlayers = Bukkit.getServer().getOnlinePlayers().size();
         if (onlinePlayers > maxOnlinePlayers) {
             maxOnlinePlayers = onlinePlayers;
-            config.set("maxOnlinePlayers", maxOnlinePlayers);
-            try {
-                config.save(configFile);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            // 可以直接用Bukkit.getServer().broadcastMessage() 但是被弃用了，因为强迫症不想看到有个警告在这里所以用遍历
-            for (Player player : Bukkit.getServer().getOnlinePlayers()) {
-                player.sendMessage("恭喜！新的最高在线人数" + maxOnlinePlayers);
-            }
+            Bukkit.getServer().broadcastMessage("恭喜！新的最高在线人数" + maxOnlinePlayers);
         }
     }
 }
