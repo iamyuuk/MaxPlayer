b1262287b00cfeed350871e1ab3d2911e74effdf
iamyuuk <2704804982@qq.com>
2023-07-22 19:26:11 +0800

    重写

--- src/main/java/org/encinet/maxplayer/MaxPlayer.java
+++ src/main/java/org/encinet/maxplayer/MaxPlayer.java
@@ -1,62 +1,76 @@
 package org.encinet.maxplayer;
+import net.md_5.bungee.api.ChatColor;
 import org.bukkit.Bukkit;
-import org.bukkit.Sound;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.Objects;
+
 public final class MaxPlayer extends JavaPlugin implements Listener {
-    private File configFile;
-    private YamlConfiguration config;
-    private int maxOnlinePlayers;
-    private boolean edit;
+
+    private int maxPlayerCount;
+
     @Override
     public void onEnable() {
-        configFile = new File(getDataFolder(), "config.yml");
-        config = YamlConfiguration.loadConfiguration(configFile);
-        if (!config.contains("maxOnlinePlayers")) {
-            config.set("maxOnlinePlayers", 0);
-        }
+        loadPlayerCount();
         getServer().getPluginManager().registerEvents(this, this);
-        Objects.requireNonNull(getCommand("maxplayers")).setExecutor(this);
-        getLogger().info("插件已开启 这是一个用来记录服务器最大在线人数的插件");
-        check();
+        Objects.requireNonNull(getCommand("/maxplayer")).setExecutor(this);
     }
+
     @Override
     public void onDisable() {
-        config.set("maxOnlinePlayers", maxOnlinePlayers);
+        savePlayerCount();
+    }
+
+    private void loadPlayerCount() {
+        File configFile = new File(getDataFolder(), "config.yml");
+        FileConfiguration config = YamlConfiguration.loadConfiguration(configFile);
+
+        if (config.contains("maxPlayerCount")) {
+            maxPlayerCount = config.getInt("maxPlayerCount");
+        } else {
+            maxPlayerCount = Bukkit.getServer().getMaxPlayers();
+        }
+    }
+
+    private void savePlayerCount() {
+        File configFile = new File(getDataFolder(), "config.yml");
+        FileConfiguration config = YamlConfiguration.loadConfiguration(configFile);
+
+        config.set("maxPlayerCount", maxPlayerCount);
+
         try {
             config.save(configFile);
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
 
-        getLogger().info("插件已关闭");
-    }
     @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent event) { check(); }
-    @Override
-    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
-        sender.sendMessage("当前最高在线人数是：" + (edit ? maxOnlinePlayers : config.getInt("maxOnlinePlayers")));
-        return true;
-    }
-    private void check() {
-        int onlinePlayers = Bukkit.getServer().getOnlinePlayers().size();
-        if (onlinePlayers > maxOnlinePlayers) {
-            maxOnlinePlayers = onlinePlayers;
-            edit = true;
-            for (Player player : Bukkit.getServer().getOnlinePlayers()) {
-                player.playSound(player.getLocation(), Sound.ENTITY_PLAYER_LEVELUP, 1.0f, 1.0f);
-                player.sendMessage("恭喜！新的最高在线人数" + maxOnlinePlayers);
-            }
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        int currentPlayerCount = Bukkit.getServer().getOnlinePlayers().size();
+
+        if (currentPlayerCount > maxPlayerCount) {
+            maxPlayerCount = currentPlayerCount;
+            savePlayerCount();
+
+            Bukkit.broadcastMessage(ChatColor.GOLD + "服务器玩家数突破新高！当前玩家数: " + currentPlayerCount);
         }
     }
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, Command command, @NotNull String label, String[] args) {
+        sender.sendMessage(ChatColor.GREEN + "当前玩家最高数: " + maxPlayerCount);
+        return true;
+    }
+
 }
